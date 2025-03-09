51b20057e391b123eb9d8337c0268595f6083add
Maksym Ryaboshapka <e.ryaboshapka777@gmail.com>
2025-03-09 12:17:20 +0200

    fix

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,7 @@
 const Link = () => {
   const linkObj = { siteName: "Google", url: "https://www.google.com/" };
 
-  return <a href={linkObj.url} target="_blank">{linkObj.siteName}</a>;
+  return <a href={linkObj.url} target="_blank" rel="noreferrer">{linkObj.siteName}</a>;
 };
 
 export default Link;
